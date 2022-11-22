3377037ce68c7d95eb9168ec34494d7595f7ce68
Ashish <113494198+svenkiah6@users.noreply.github.com>
2022-11-22 15:34:22 +0400

    Create playRound function
    This function will return the outcome of the game based on parameters
    passed

--- script.js
+++ script.js
@@ -4,7 +4,61 @@ const CHOICES = ['rock', 'paper', 'scissors']
 function playRound(playerSelection, computerSelection)
 {
 
-   
+    let actualPlayerChoice = playerSelection.toLowerCase()
+
+
+
+
+    if(actualPlayerChoice=="rock")
+    {
+        if(computerSelection=="paper")
+        {
+            return "You lose! Paper beats Rock"
+        }
+        
+        if(computerSelection=="scissors")
+        {
+            return "You win! Rock beats Scissors"
+        } 
+
+            return "Draw!"
+        
+    }
+
+    if(actualPlayerChoice=="scissors")
+    {
+        if(computerSelection=="paper")
+        {
+            return "You win! Scissors beats Paper"
+        }
+        
+        if(computerSelection=="rock")
+        {
+            return "You lose! Rock beats Scissors"
+        } 
+
+            return "Draw!"
+    }
+
+
+    if(actualPlayerChoice=="paper")
+    {
+        if(computerSelection=="rock")
+        {
+            return "You win! Paper beats Rock"
+        }
+        
+        if(computerSelection=="scissors")
+        {
+            return "You lose! Scissors beats Paper"
+        } 
+
+            return "Draw!"
+    }
+
+    return "Invalid choice"
+
+
 
     
 }
