9e91d855649c20eb6668bb2057b18bedabc2a641
Ashish <113494198+svenkiah6@users.noreply.github.com>
2022-11-22 14:16:49 +0400

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,26 @@
+const CHOICES = ['rock', 'paper', 'scissors']
+
+
+function playRound(playerSelection, computerSelection)
+{
+
+   
+
+    
+}
+
+function getComputerChoice()
+{
+
+    return CHOICES[getRandomInt(3)]
+
+}
+
+
+
+function getRandomInt(max) {
+    
+    return Math.floor(Math.random() * max);
+}
+
+
